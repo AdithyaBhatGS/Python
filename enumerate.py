aec42c560d02f076254defd073255489f47c4a46
Adithya Bhat G S <adithyabhatgs@gmail.com>
2022-12-15 18:41:50 +0530

    enumerate.py modified

--- enumerate.py
+++ enumerate.py
@@ -3,19 +3,30 @@
 
 list1=['amith','ashok','ankita','sumanth','raghavendra','ram','sita']
 
-for i,j in enumerate(list1):
-    print(i,end=',')
-    print(j) 
+# for i,j in enumerate(list1):
+    # print(i,end=',')
+    # print(j) 
+# or
+# enumerate()-> adds a counter to each elements in iterables and returns an enumerating object
+# list(enum_obj)->list of tuples with each tuple inside the list containing an iterable and a value
+
+obj1=enumerate(list1)
+print(list(obj1))
 
 str1='Ashika'
+obj2=enumerate(str1)
+print(list(obj2))
 
-for i,j in enumerate(str1):
-    print(i,end=',')
-    print(j)
-
+# for i,j in enumerate(str1):
+    # print(i,end=',')
+    # print(j)
+# 
 dict1={'Dosa':'3qty','Idly':'5qty','Pav Bhaji':'3qty'}
 
-for i,j in enumerate(dict1.items()):
-    print(i,end=',')
-    print(j[0],end=',')
-    print(j[1])
+obj3=enumerate(dict1)
+print(list(obj3))
+
+# for i,j in enumerate(dict1.items()):
+    # print(i,end=',')
+    # print(j[0],end=',')
+    # print(j[1])
